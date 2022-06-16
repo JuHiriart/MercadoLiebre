6c70cc5ca2f4a319110f118e471088dad8de39d0
SantiagoHiriart <hiriart1234@gmail.com>
2022-06-16 18:43:25 -0300

    mi primer commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web_ node source/app.js
---
+++ source/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const port = process.env.PORT || 2020; //para heroku
+
+app.listen(port, () => console.log('Listening on http://localhost:'+port));
+//app.listen(3000, () => console.log("Servidor en marcha"))
+
+
+
+const public = path.resolve(__dirname, '../public');
+app.use(express.static(public));                                          //tengo acceso a lo que tengo en la carpeta public con estas lineas
+
+
+app.get('/', function (req, res){
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+})
